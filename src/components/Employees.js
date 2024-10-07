ad73e8ed2b1a382d5c6dc2824bc8416b412fcbc5
Hamsa M <hamsaganesh21@gmail.com>
2024-10-07 14:54:57 +0530

    reactcrudapp

---
+++ src/components/Employees.js
@@ -0,0 +1,19 @@
+const Employees = [
+    {
+       id:"1",
+       name: "abc",
+       age:"30"
+    },
+    {
+        id:"2",
+        name: "xyz",
+        age:"24"
+     },
+     {
+        id:"3",
+        name: "pqr",
+        age:"35"
+     }
+]
+
+export default Employees;
